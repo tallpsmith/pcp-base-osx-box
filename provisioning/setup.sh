0f750cc6f0c8bad3774834c1856914f10f5f60a1
tallpsmith <psmith@aconex.com>
2017-02-28 23:32:21 +1100

    Updated provisioning scripts based on PCP learnings and zero out the empty space for slightly better compression.
    Every GB counts..

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
+
+# Ensure OSX has a `vagrant:vagrant` style user:group
+# otherwise rsync doesn't work properly...
+VAGRANT_GID=$(id -u vagrant)
+sudo dscl . -create /Groups/vagrant
+sudo dscl . -create /Groups/vagrant PrimaryGroupID $VAGRANT_GID
+sudo dscl . -create /Users/vagrant PrimaryGroupID $VAGRANT_GID
+
+
 sudo -H -u vagrant brew update
-sudo -H -u vagrant brew install --universal libmicrohttpd pixman  libpng xz
-sudo -H -u vagrant brew install cairo coreutils automake
-sudo -H -u vagrant brew install qt pyqt5
+sudo -H -u vagrant brew upgrade
+sudo -H -u vagrant brew install --universal coreutils automake libmicrohttpd pixman libpng xz
+sudo -H -u vagrant brew install pkg-config cairo qt pyqt5
 sudo -H -u vagrant brew install gnu-tar --with-default-names
+
+sudo -H -u vagrant brew cleanup
+
+sudo dd if=/dev/zero of=/EMPTY bs=1m || true; sudo sync; sudo rm -f /EMPTY; sudo sync
